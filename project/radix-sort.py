18a95a59665b0efef4c196570bf7d28d1b5180ef
msummerville2002 <msummerville@hawk.iit.edu>
2021-05-05 13:10:55 -0500

    started project

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -1,10 +1,15 @@
 import urllib
 import requests
-
+setx PATH "%PATH%;C:\Python33\Scripts"
 def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     booktxt = urllib.request.urlopen(book_url).read().decode()
     bookascii = booktxt.encode('ascii','replace')
     return bookascii.split()
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
+    pass
+
+def radix_sort(self):
+    pass 
+def calc_word_lenest(self):
     pass
