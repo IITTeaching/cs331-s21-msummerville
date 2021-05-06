a436f36a3204330ea0fb0ce50efa9120ed0cf895
msummerville2002 <msummerville@hawk.iit.edu>
2021-05-06 13:28:44 -0500

    more work on radix

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -9,39 +9,31 @@ def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     pass
 
-def counting_sort(words, k):
-    len_words = len(words) 
+def itemsToQueues(words,i):
+    q = [None] * 256
+    for j in range(0,256):
+        q[j] = []
+    for k in range(0,len(words)):
+        index = len(words[k])-i-1
+        if(len(words[k]) < index):
+            q[0].append(words[k])
+        else:
+            q[words[k][index]].append(words[k])
+    return q
 
-    output = [None] * len_words
-
-    count = [None] * 10
-
-    for i in range(0, len_words):
-        index = (words[i] / k)
-        count[int(index % 10)] += 1
-
-    for i in range(1, 10):
-        count[i] += count[i - 1]
-    
-    i = len_words - 1
-    while i >= 0:
-        index = (words[i] / k)
-        output[count[int(index % 10)] - 1] = words[i]
-        count[int(index % 10)] -= 1
-        i -= 1
-    i = 0
-
-    for i in range(0, len(words)):
-        words[i] = output[i]
+def queuesToArray(ques,numWords):
+    c = [None] * numWords
+    count = 0
+    for i in range(0,len(ques)):
+        
 
 
-def radix_sort(self,):
+def radix_sort():
     lenest_word = calc_lenest_word()
     words = book_to_words()
-    k = 1
-    while lenest_word / k > 0:
-        counting_sort(words,k)
-        k *= 10
+    for i in range(0,lenest_word):
+        c = itemsToQueues(words,i)
+        final = queuestoArray(c,len(words))
 
 def calc_lenest_word():
     maxlen = 1
@@ -50,3 +42,4 @@ def calc_lenest_word():
         if(len(i) > maxlen):
             maxlen = len(i)
     return maxlen
+radix_sort()
