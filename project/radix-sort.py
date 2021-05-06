95ef60edd1b23692cdb00dddd4b77b6f1e44bd81
Blase <fuckoff>
2021-05-06 02:22:16 -0500

    improved on radix

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -1,6 +1,6 @@
 import urllib
 import requests
-setx PATH "%PATH%;C:\Python33\Scripts"
+import 
 def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     booktxt = urllib.request.urlopen(book_url).read().decode()
     bookascii = booktxt.encode('ascii','replace')
@@ -10,6 +10,13 @@ def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     pass
 
 def radix_sort(self):
-    pass 
-def calc_word_lenest(self):
-    pass
+    len_word = calc_lenest_word()
+    for i in range(0,len_word)
+        
+def calc_lenest_word():
+    maxlen = 1
+    lst = book_to_words()
+    for i in lst:
+        if(len(i) > maxlen):
+            maxlen = len(i)
+    return maxlen
